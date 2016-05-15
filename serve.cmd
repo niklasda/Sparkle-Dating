92def2fc8e89b1a9d07732a99a21904fc28ae397
Niklas Dahlman <nd@sql8r.net>
2016-05-15 22:36:35 +0200

    command line to serve app

---
+++ serve.cmd
@@ -0,0 +1,10 @@
+G:
+cd G:\Projects\GitHub\Sparkle-Dating
+cd G:\Projects\GitHub\Sparkle-Dating\Sparkle-Dating-App
+
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window http://localhost:8000/browser/www/
+
+REM "D:\Program Files (x86)\Mozilla Firefox\firefox.exe" -height 800 -width 480 http://localhost:8000/browser/www/
+
+REM "C:\Program Files\Internet Explorer\iexplore.exe"  http://localhost:8000/browser/www/
+
